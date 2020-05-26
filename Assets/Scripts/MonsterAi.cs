34313fa989fbba0cf0d5ce8c1ab696f3f7f6300f
Kyon_dreamer <dream7652210@gmail.com>
2020-05-27 06:34:24 +0800

    5/27  等待射擊放入場景中

---
+++ Assets/Scripts/MonsterAi.cs
@@ -0,0 +1,101 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class MonsterAi : MonoBehaviour
+{
+    public Animator wolfAni;
+
+    // 待機 , 追擊 , 攻擊的清單
+    public enum Status { idle, chase, attack };
+    public Status status;
+
+    // 怪物面向
+    public enum Face { Right, Left };
+    public Face face;
+
+
+    public float speed;
+    public Transform monster;
+    private SpriteRenderer spr;
+
+    public Transform player;
+
+
+    void Start()
+    {
+        wolfAni= GameObject.Find("Wolf (1)").GetComponent<Animator>();
+
+        status = Status.idle;
+
+        if (player.transform.position.x > monster.transform.position.x) 
+        {
+            face = Face.Left;
+        }
+        else
+        {
+            face = Face.Right;
+        }
+    }
+
+    void Update()
+    {
+        float dTime = Time.deltaTime;
+        
+
+        switch (status)
+        {
+            case Status.idle:          
+                if (Mathf.Abs(monster.position.x - player.position.x) < 5 && Mathf.Abs(monster.position.y - player.position.y) < 2)
+                {
+                    if (wolfAni.GetInteger("runW") == 0)
+                        wolfAni.SetInteger("runW", 1);
+                   
+                    //-------咆哮動畫-------//
+
+                    //-------咆哮動畫-------//
+                    status = Status.chase;
+                }
+                break;
+
+            case Status.chase:
+                if (monster.position.x >= player.position.x)
+                {
+                    face = Face.Left;
+                }
+                else
+                {
+                    face = Face.Right;
+                }
+
+                switch (face)
+                {
+                    case Face.Left:
+                        monster.transform.localScale = new Vector2(1, 1);
+                        transform.position -= new Vector3(speed * dTime, 0, 0);
+                        break;
+                    case Face.Right:
+                        monster.transform.localScale = new Vector2(-1, 1);
+                        transform.position += new Vector3(speed * dTime, 0, 0);
+                        break;
+                }
+                if (Mathf.Abs(monster.position.x - player.position.x) >= 6)
+                {
+                    if (wolfAni.GetInteger("runW") == 1)
+                        wolfAni.SetInteger("runW", 0);
+                    status = Status.idle;
+                }
+                break;
+            case Status.attack:
+                if (Mathf.Abs(monster.position.x - player.position.x) < 2)
+                {
+
+                    //-------攻擊-----//
+
+                    //-------攻擊-----//
+
+                }
+                break;
+        }
+    }
+}
--- Assets/Scripts/ShootController.cs
+++ Assets/Scripts/ShootController.cs
@@ -5,75 +5,81 @@ using UnityEngine.UI;
 
 public class ShootController : MonoBehaviour
 {
-
     public GameObject slingshot; // 彈弓
-    public static float angle = 60f; // 角度
+    public float angle; // 角度
     public GameObject bullet; // 子彈
-    public Transform rayPic;
-    public float power = 500f;
-    public GameObject rayL;
+    public Transform ray;
+    public Transform player;
+    public Camera camera;
+
+    //---------------------------------------------------------//
+    private float crtForce = 0; // 當前的力
+    private float minForce = 50; // 最小力
+    private float maxForce = 200; // 最大力
+    private float forceSpeed = 100; // 蓄力速度
 
 
-    void Start()
-    {
-       rayPic.eulerAngles = new Vector3(0, 0, angle);       
-    }
-
-    // Update is called once per frame
     void Update()
     {
-        shootControllor();
+        lookAtMouse();
+
+        ray.transform.eulerAngles = new Vector3(0, 0, angle);
+
+        if (Input.GetMouseButton(0))
+        {
+            ray.transform.localScale = new Vector3((crtForce / 120), 1 - (crtForce / 360), 1);
+            crtForce += forceSpeed * Time.deltaTime; // 蓄力
+
+            if (crtForce >= maxForce)
+            {
+                crtForce = maxForce;
+                if (Input.GetMouseButtonUp(0))
+                {
+                    shoot();
+                    crtForce = 50;
+                }
+            }
+        }
+        if (Input.GetMouseButtonUp(0))
+        {
+            shoot();
+            ray.transform.localScale = new Vector3(1, 1, 1);
+            crtForce = 50;
+        }
+
+        // 調整轉向箭頭貼圖錯誤的問題
+        if (player.localScale.x > 0)
+        {
+            if (angle >= 90)
+            {
+                angle = 90;
+            }
+            slingshot.transform.localScale = new Vector3(1, 1, 1);
+        }
+        else if (player.localScale.x < 0)
+        {
+            if (angle <= 85)
+            {
+                angle = 85;
+            }
+            slingshot.transform.localScale = new Vector3(-1, 1, 1);
+        }
+
+       // print(crtForce);
     }
 
-    private void shootControllor()
-    {
-        if (Input.GetKeyDown(KeyCode.Z))
-        {
-            rayL.SetActive(true);
-        }
-        if (Input.GetKeyDown(KeyCode.Z))
-        {
-            shoot(power);
-        }
-        else if (Input.GetKeyDown(KeyCode.X))
-        {
-            rayL.SetActive(false);
-        }
-
-        if (Input.GetKey(KeyCode.UpArrow))
-        {
-            if (slingshot)
-            {
-                angle += 0.5f;
-                angle = angle % 360;
-            }
-            else
-            {
-                angle -= 0.5f;
-                angle = angle % 360;
-            }
-            rayPic.eulerAngles = new Vector3(0, 0, angle);
-        }
-        if (Input.GetKey(KeyCode.DownArrow))
-        {
-            if (!slingshot)
-            {
-                angle += 0.5f;
-                angle = angle % 360;
-            }
-            else
-            {
-                angle -= 0.5f;
-                angle = angle % 360;
-            }
-            rayPic.eulerAngles = new Vector3(0, 0, angle);
-        }
-    }
-    private void shoot(float power)
+    private void shoot()
     {
         GameObject _bullet = Instantiate(bullet, slingshot.transform.position, Quaternion.identity) as GameObject;
         Vector2 forceDir = new Vector2(Mathf.Cos(Mathf.PI * angle / 180), Mathf.Sin(Mathf.PI * angle / 180));
-        _bullet.GetComponent<Rigidbody2D>().AddForce(forceDir * power);
+        _bullet.GetComponent<Rigidbody2D>().AddForce(forceDir * crtForce);
+    }
+
+    void lookAtMouse()
+    {
+        Vector3 dir = Input.mousePosition - camera.WorldToScreenPoint(transform.position);
+        angle = Mathf.Atan2(dir.y, dir.x) * Mathf.Rad2Deg;
+        transform.rotation = Quaternion.AngleAxis(angle, Vector3.forward);
     }
 
 }
