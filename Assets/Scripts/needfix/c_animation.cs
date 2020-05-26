505e66aa03dd529ef692831a87571874e36bff22
Kyon_dreamer <dream7652210@gmail.com>
2020-05-26 23:14:54 +0800

    5/26 水井備份

---
+++ Assets/Scripts/needfix/c_animation.cs
@@ -0,0 +1,28 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class c_animation : MonoBehaviour
+{
+    public GameObject obj;
+    private Animator ani;
+    // Start is called before the first frame update
+    void Start()
+    {
+        ani = obj.GetComponent<Animator>();
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+
+    }
+
+    private void OnTriggerStay2D(Collider2D collision)
+    {
+        if (Input.GetKeyDown(KeyCode.F) && collision.tag == "Player")
+        {
+            ani.SetBool("switch", true);
+        }
+    }
+}
--- Assets/Scripts/needfix/change_cine.cs
+++ Assets/Scripts/needfix/change_cine.cs
@@ -4,12 +4,17 @@ using UnityEngine;
 
 public class change_cine : MonoBehaviour
 {
-    // Start is called before the first frame update
     public List<GameObject> cam;
-    public GameObject cam_s;
+    public GameObject cam_a, cam_s, cam_e;
+
+    private bool back = false;
     void Start()
     {
-        
+        cam = new List<GameObject>();
+        for (int i = 0; i <= cam_a.transform.childCount - 1; i++)
+        {
+            cam.Add(cam_a.transform.GetChild(i).gameObject);
+        }
     }
 
     // Update is called once per frame
@@ -19,14 +24,25 @@ public class change_cine : MonoBehaviour
     }
     private void OnTriggerEnter2D(Collider2D collision)
     {
-        if (collision.tag == "Player")
+        if (collision.tag == "Player" &&　back == false)
         {
             for (int i = 0; i <= cam.Count-1; i++)
             {
                 cam[i].SetActive(false);
             }
 
+            back = true;
             cam_s.SetActive(true);
+        }
+        else if (collision.tag == "Player" && back == true)
+        {
+            for (int i = 0; i <= cam.Count - 1; i++)
+            {
+                cam[i].SetActive(false);
+            }
+
+            back = false;
+            cam_e.SetActive(true);
         }
     }
 }
