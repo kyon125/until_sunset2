3ef00079a0455828b69f2547e9a219ddd0e1fb54
Kyon_dreamer <dream7652210@gmail.com>
2020-06-02 16:13:12 +0800

    6/2

--- Assets/Scripts/Common/Dead_setting.cs
+++ Assets/Scripts/Common/Dead_setting.cs
@@ -2,18 +2,24 @@
 using System.Collections.Generic;
 using UnityEngine;
 using UnityEngine.UI;
+using UnityEngine.SceneManagement;
 using DG.Tweening;
 
 public class Dead_setting : MonoBehaviour
 {
     Text tile , start , end;
+    Button b1, b2;
     // Start is called before the first frame update
     void Start()
     {
         tile = GameObject.Find("tile").GetComponent<Text>();
         start = GameObject.Find("T_restart").GetComponent<Text>();
         end = GameObject.Find("T_end").GetComponent<Text>();
+
+        b1 = GameObject.Find("restart").GetComponent<Button>();
+        b1 = GameObject.Find("end").GetComponent<Button>();
         StartCoroutine(show());
+               
     }
 
     // Update is called once per frame
@@ -21,14 +27,23 @@ public class Dead_setting : MonoBehaviour
     {
         
     }
+    public void restatgame()
+    {
+        SceneManager.LoadScene("Well_01");
+    }
+    public void endgame()
+    {
+        SceneManager.LoadScene("Start");
+    }
 
     IEnumerator show()
     {
         yield return new WaitForSeconds(0.5f);
         Tween t = tile.transform.DOScaleX(1, 0.3f);
+
         yield return new WaitForSeconds(0.5f);
-        Tween t2 = start.DOFade(255, 0.1f);
-        Tween t3 = end.DOFade(255, 0.1f);
+        Tween t2 = start.DOFade(255, 2f);
+        Tween t3 = end.DOFade(255, 2f);
 
     }
 }
