cfb6aaad1a62e9f697cb859bcb4b3f4abe734146
Kyon_dreamer <dream7652210@gmail.com>
2020-11-04 01:33:53 +0800

    11/4

---
+++ Assets/Scripts/well/area_A/rock_falldown.cs
@@ -0,0 +1,34 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class rock_falldown : MonoBehaviour
+{
+    // Start is called before the first frame update
+    public GameObject rock;
+    public simplot plot;
+    public int waittime;
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+    private void OnTriggerEnter2D(Collider2D collision)
+    {
+        if (collision.tag == "Player")
+        {
+            StartCoroutine(wait());
+            Destroy(rock);
+        }
+    }
+
+    IEnumerator wait()
+    {
+        yield return new WaitForSeconds(waittime);
+    }
+}
