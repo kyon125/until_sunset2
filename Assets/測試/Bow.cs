1b6e71e5139685cdffb8642450130b0e56a85168
MinamiSaku <37174754+MinaKaru@users.noreply.github.com>
2020-12-15 17:37:14 +0800

    弓箭合併

--- Assets/測試/Bow.cs
+++ Assets/測試/Bow.cs
@@ -6,68 +6,46 @@ using UnityEngine;
 public class Bow : MonoBehaviour
 {
     bool shot = false;
-
-    public GameObject player;
-    public GameObject bow;
-    public bool canRotate;
-    Vector2 mousePosition;
+    bool canRotate;
+    Vector3 mousePosition;
 
     public Camera camera;
+    public GameObject shotPos;
     public GameObject arrow;
     public float launchForce;
     public Transform shotPoint;
 
+    public GameObject bow;
     public GameObject point;
     GameObject[] points;
     public int numberOfPoints;
     public float spaceBetweenPoints;
-    Vector2 dircetion;
+    Vector2 direction;
+    public GameObject player;
 
-    private void Start()
+    void Start()
     {
         points = new GameObject[numberOfPoints];
         for (int i = 0; i < numberOfPoints; i++)
-        {        
+        {
             points[i] = Instantiate(point, shotPoint.position, Quaternion.identity);
         }
+
+        StartCoroutine("hidePoints");
     }
 
+    // Update is called once per frame
     void Update()
     {
-        Vector2 bowPosition = transform.position;
-        mousePosition = Input.mousePosition;
-        dircetion = mousePosition - bowPosition;
+        faceMouse();
+        rotateLim();
 
-
-        if (Input.GetMouseButtonDown(1) && shot == false)
+        if (Input.GetMouseButtonDown(1))
         {
-            shot = true;
-            bow.SetActive(true);
-            for (int i = 0; i < numberOfPoints; i++)
-                points[i].GetComponent<SpriteRenderer>().enabled = true;
-
-        }
-        else if (Input.GetMouseButtonDown(1) && shot == true)
-        {
-            shot = false;
-            bow.SetActive(false);
-            for (int i = 0; i < numberOfPoints; i++)
-                points[i].GetComponent<SpriteRenderer>().enabled = false;
+            StartCoroutine("bowUse");
         }
 
-        // bow校正
-        if (player.transform.localScale.x > 0)
-        {
-            this.gameObject.transform.localScale = new Vector3(1, 1, 1);
-        }
-        // bow校正
-        else if (player.transform.localScale.x < 0)
-        {
-            this.gameObject.transform.localScale = new Vector3(-1, 1, 1);
-        }
-
-
-        if ((player.transform.localScale.x > 0 && dircetion.x < 0) || (player.transform.localScale.x < 0 && dircetion.x > 0))
+        if ((player.transform.localScale.x > 0 && direction.x < 0) || (player.transform.localScale.x < 0 && direction.x > 0))
         {
             canRotate = false;
         }
@@ -78,32 +56,82 @@ public class Bow : MonoBehaviour
 
         if (canRotate == true)
         {
-            mousePosition = camera.ScreenToWorldPoint(Input.mousePosition);
-            transform.right = dircetion;
-
-            if (Input.GetMouseButtonDown(0) && shot == true)
-            {
-                Shoot();
-            }
+            shotPos.transform.position = mousePosition;
+            Shoot();
 
             for (int i = 0; i < numberOfPoints; i++)
             {
                 points[i].transform.position = PonitPosition(i * spaceBetweenPoints);
             }
         }
+        //print(direction);
+    }
+
+    void faceMouse()
+    {
+        mousePosition = Input.mousePosition;
+        mousePosition = camera.ScreenToWorldPoint(mousePosition);
+
+        direction = new Vector2(mousePosition.x - transform.position.x, mousePosition.y - transform.position.y);
+
+        transform.right = direction;
     }
 
     private void Shoot()
     {
-        GameObject newArrow = Instantiate(arrow, shotPoint.position, shotPoint.rotation);   
-        newArrow.GetComponent<Rigidbody2D>().velocity = transform.right * launchForce;
+        if (Input.GetMouseButtonDown(0) && shot)
+        {
+            GameObject newArrow = Instantiate(arrow, shotPoint.position, shotPoint.rotation);
+            newArrow.GetComponent<Rigidbody2D>().velocity = transform.right * launchForce;
+        }
+    }
 
-
+    private void rotateLim()
+    {
+        // bow校正
+        if (player.transform.localScale.x > 0)
+        {
+            this.gameObject.transform.localScale = new Vector3(1, 1, 1);
+        }
+        // bow校正
+        else if (player.transform.localScale.x < 0)
+        {
+            this.gameObject.transform.localScale = new Vector3(-1, 1, 1);
+        }
     }
 
     Vector2 PonitPosition(float t)
     {
-        Vector2 position = (Vector2)shotPoint.position + (dircetion.normalized * launchForce * t) + 0.5f * Physics2D.gravity * (t * t);
+        Vector2 position = (Vector2)shotPoint.position + (direction.normalized * launchForce * t) + 0.5f * Physics2D.gravity * (t * t);
         return position;
+    }
+
+    IEnumerator bowUse()
+    {
+        yield return null;
+
+        if (shot == false)
+        {
+            shot = true;
+            bow.SetActive(true);
+            for (int i = 0; i < numberOfPoints; i++)
+                points[i].GetComponent<SpriteRenderer>().enabled = true;
+
+        }
+        else if (shot == true)
+        {
+            shot = false;
+            bow.SetActive(false);
+            for (int i = 0; i < numberOfPoints; i++)
+                points[i].GetComponent<SpriteRenderer>().enabled = false;
+        }
+    }
+
+    IEnumerator hidePoints()
+    {
+        yield return (1);
+
+        for (int i = 0; i < numberOfPoints; i++)
+            points[i].GetComponent<SpriteRenderer>().enabled = false;
     }
 }
