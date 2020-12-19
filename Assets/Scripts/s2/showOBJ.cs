272ec40f2a07ac55756e2d4ee3767b5303875b3f
Kyon_dreamer <dream7652210@gmail.com>
2020-12-19 23:55:58 +0800

    樹的關卡雛形

---
+++ Assets/Scripts/s2/showOBJ.cs
@@ -0,0 +1,46 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class showOBJ : MonoBehaviour
+{
+    // Start is called before the first frame update
+    public GameObject morning, evening, night;
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        show();       
+    }
+    void show()
+    {
+        switch (GameStatus.gameStatus.gametime)
+        {
+            case (GameStatus.Gametime.morning):
+                {
+                    morning.SetActive(true);
+                    evening.SetActive(false);
+                    night.SetActive(false);
+                    break;
+                }
+            case (GameStatus.Gametime.evening):
+                {
+                    morning.SetActive(false);
+                    evening.SetActive(true);
+                    night.SetActive(false);
+                    break;
+                }
+            case (GameStatus.Gametime.night):
+                {
+                    morning.SetActive(false);
+                    evening.SetActive(false);
+                    night.SetActive(true);
+                    break;
+                }
+        }
+    }
+}
