9e3478deb1772f8109d7224b6f66688229852f4a
Kyon_dreamer <dream7652210@gmail.com>
2020-12-22 03:50:34 +0800

    S3場景拉完 功能實施一半

---
+++ Assets/Scripts/s3/cloudBasic.cs
@@ -0,0 +1,47 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using DG.Tweening;
+
+public class cloudBasic : MonoBehaviour
+{
+    // Start is called before the first frame update
+    public float pos ,down ,dt;
+    bool isplayer, ismove;
+    LayerMask player;
+    void Start()
+    {
+        pos = transform.position.y;
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+    private void OnCollisionEnter2D(Collision2D collision)
+    {
+        if (collision.transform.tag == "Player" && ismove == false)
+        {
+            Boing();
+            ismove = true;
+        }
+    }
+    private void OnCollisionExit2D(Collision2D collision)
+    {
+        if (collision.transform.tag == "Player" && ismove == false)
+        {
+            transform.DOLocalMoveY(pos, 0.5f);
+        }
+    }
+    void Boing()
+    {
+        StartCoroutine(waitboing());
+    }
+    IEnumerator waitboing()
+    {
+        transform.DOBlendableMoveBy(new Vector3(0, -down, 0), dt).SetEase(Ease.OutBack);
+        yield return new WaitForSeconds(dt);
+        ismove = false;
+    }
+}
---
+++ Assets/Scripts/s3/cloudMove.cs
@@ -0,0 +1,119 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using DG.Tweening;
+
+public class cloudMove : MonoBehaviour
+{
+    // Start is called before the first frame update
+    public Direction direction;
+    public Selftime selftime;
+    public List<move> list;
+    GameStatus.Gametime GT;
+    void Start()
+    {
+        intial();
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        if (GT != GameStatus.gameStatus.gametime)
+        {
+            change();
+            GT = GameStatus.gameStatus.gametime;
+        }
+        GT = GameStatus.gameStatus.gametime;
+    }
+    void intial()
+    {
+        switch (GameStatus.gameStatus.gametime)
+        {
+            case (GameStatus.Gametime.morning):
+                {                    
+                    selftime = Selftime.M;
+                    gohead();
+                    break;
+                }
+            case (GameStatus.Gametime.evening):
+                {
+                    selftime = Selftime.E;
+                    gohead();
+                    break;
+                }
+            case (GameStatus.Gametime.night):
+                {
+                    selftime = Selftime.N;
+                    gohead();
+                    break;
+                }
+        }
+    }
+    void change()
+    {
+        switch (GameStatus.gameStatus.gametime)
+        {
+            case (GameStatus.Gametime.morning):
+                {
+                    selftime = Selftime.M;
+                    gohead();
+                    break;
+                }
+            case (GameStatus.Gametime.evening):
+                {
+                    selftime = Selftime.E;
+                    gohead();
+                    break;
+                }
+            case (GameStatus.Gametime.night):
+                {
+                    selftime = Selftime.N;
+                    gohead();
+                    break;
+                }
+        }
+    }
+    void gohead()
+    {
+        for (int i = 0; i < list.Count; i++)
+        {
+            if (list[i].s == selftime)
+            {
+                transform.DOLocalMove(list[i].pos, list[i].time);
+            }
+        }
+        
+    }
+    private void OnCollisionEnter2D(Collision2D collision)
+    {
+        if (collision.transform.tag == "Player")
+        {
+            collision.transform.parent = transform;
+        }
+    }
+    private void OnCollisionExit2D(Collision2D collision)
+    {
+        if (collision.transform.tag == "Player")
+        {
+            collision.transform.parent = GameObject.Find("GameController").transform;
+        }
+    }
+    public enum Direction
+    {
+        Horizontal,
+        vertical
+    }
+    public enum Selftime
+    {
+        M,
+        E,
+        N
+    }
+    [System.Serializable]
+    public class move
+    {
+        public Selftime s;
+        public Vector3 pos;
+        public float time;
+    }
+}
