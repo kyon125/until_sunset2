5c23f2e33079233dad359f69a1278959364b3904
Kyon_dreamer <dream7652210@gmail.com>
2020-12-23 02:44:14 +0800

    水井大部分bug修完

---
+++ Assets/Scripts/Common/AutoSave.cs
@@ -0,0 +1,22 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class AutoSave : MonoBehaviour
+{
+    // Start is called before the first frame update
+    private void Awake()
+    {
+        
+    }
+    void Start()
+    {
+        saveGame.savecontroller.save();
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+}
